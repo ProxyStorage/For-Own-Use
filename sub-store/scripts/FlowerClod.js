58b4742a25717d4919a6d89354273da5f83745a7
DreamyTZK <i@tzki.cn>
2022-04-17 19:37:05 +0800

    ✨ feat: flower脚本和prpr脚本

--- sub-store/scripts/FlowerClod.js
+++ sub-store/scripts/FlowerClod.js
@@ -6,7 +6,6 @@
 
 // 机场节点修饰前缀
 const prefix = '[Flower]'
-
 /**
  * 入口函数
  * @param {{name:string}[]} proxies
@@ -14,8 +13,33 @@ const prefix = '[Flower]'
  */
 function operator(proxies) {
   return proxies.map((p) => {
-    p.name = prefix + p.name
     p.name = p.name.replace(' IEPL 中继 ', ' ')
+    p.name = replaceLocation(p.name)
+    p.name = prefix + p.name
     return p
   })
+}
+
+/**
+ * 替换地区
+ * @param {string} name
+ */
+function replaceLocation(name) {
+  let resultName = name
+  if (name.match(/香港/)) {
+    resultName = name.replace('香港', 'HK ')
+  } else if (name.match(/台湾/)) {
+    resultName = name.replace('台湾', 'TW ')
+  } else if (name.match(/日本/)) {
+    resultName = name.replace('日本', 'JP ')
+  } else if (name.match(/韩国/)) {
+    resultName = name.replace('韩国', 'KR ')
+  } else if (name.match(/新加坡/)) {
+    resultName = name.replace('新加坡', 'SG ')
+  } else if (name.match(/美国/)) {
+    resultName = name.replace('美国', 'US ')
+  } else {
+    resultName = name
+  }
+  return resultName
 }
